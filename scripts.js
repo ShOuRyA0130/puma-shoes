7c30dfd9689200b898c0ff6989eb9c14dbbec9a4
ShOuRyA0130 <shouryagupta286@gmail.com>
2024-07-16 07:39:06 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function buyNow() {
+    alert("Thank you for your purchase!");
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for contacting us!");
+});
+
+
